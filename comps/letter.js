111b4d27f4fba19d66fe0f730606cb451cfcf4c3
Joshua Galvin <joshuagalvin@gmail.com>
2015-04-12 16:53:00 -0700

    moar

--- comps/letter.js
+++ comps/letter.js
@@ -1,12 +1,17 @@
 var m = require("mithril");
 
 module.exports = function(ctrl) {
-    ctrl.letter = true;
+    ctrl.letter = "render";
+    setTimeout(function() {
+        ctrl.letter = "opener";
+        m.redraw(true);
+    }, 300);
     return function(ctrl) {
         return ctrl.letter ? m(".letter", {
             onclick : function() {
-                ctrl.letter = false;
-            }
+                ctrl.letter = "dontcare";
+            },
+            class : ctrl.letter
         }, m.trust("Congratulations on getting into college, sweetie! <br/><br/>You’re finally all moved into your first apartment and all ready for school, and I have never been more proud of you.<br/><br/> So you don’t worry, I paid off your first month’s rent. Be sure to call and tell me everything that happens. <br/><br/>Best of luck! Love you! <br/>" +
                "- Mom")) : "";
     };
